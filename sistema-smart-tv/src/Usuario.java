e11ee3c5c9f88f8deae7cb9666faeae037544559
nathanribeiro05 <jonathanribeiro05@gmail.com>
2023-03-28 19:27:05 -0300

    mudança teste

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -2,7 +2,6 @@ public class Usuario {
     public static void main(String[] args) throws Exception {
         SmartTv smartTv = new SmartTv();
 
-        System.out.println("Status atual da TV:");
         System.out.println("TV ligada? " + smartTv.ligada);
         System.out.println("Canal atual: " + smartTv.canal);
         System.out.println("Volume atual: " + smartTv.volume);

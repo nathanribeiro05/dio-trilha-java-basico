3e4a16db8bdeacb0c4b32c9a665a44fffdd87a10
nathanribeiro05 <jonathanribeiro05@gmail.com>
2023-03-30 14:13:25 -0300

    desafio fabrica de carro java

---
+++ desafio-fabrica-carros/src/CalculoPreço.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CalculoPreço {
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Insira os dados: ");
+        int custoFabrica = scan.nextInt();
+        int porcentagemDistribuidor = scan.nextInt();
+        int porcentagemImpostos = scan.nextInt();
+
+        int valorImpostos = (custoFabrica * (porcentagemImpostos/100));
+        int valorDistribuidor = (custoFabrica * (porcentagemDistribuidor/100));
+
+        System.out.println("O custo final do carro é: " + (custoFabrica + valorDistribuidor + valorImpostos));
+
+        scan.close();
+    }
+}

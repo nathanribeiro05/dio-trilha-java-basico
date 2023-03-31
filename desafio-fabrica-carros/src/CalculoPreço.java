d394d516d5b5493cd117c51bf79ac6a518f5aa04
nathanribeiro05 <jonathanribeiro05@gmail.com>
2023-03-31 09:44:25 -0300

    desafio fabrica carros java

--- desafio-fabrica-carros/src/CalculoPreço.java
+++ desafio-fabrica-carros/src/CalculoPreço.java
@@ -1,3 +1,10 @@
+//Você receberá o custo de fábrica e as porcentagens referentes ao distribuidor e os impostos
+//e deverá escrever programa para ler esses valores e imprimir o valor final do carro.
+//Entrada	Saída
+//20000     34600
+//28
+//45	
+
 import java.util.Scanner;
 
 public class CalculoPreço {

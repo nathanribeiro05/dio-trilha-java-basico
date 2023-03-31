5014cbf76285a5c6fbd338df7e8e22bce453e280
nathanribeiro05 <jonathanribeiro05@gmail.com>
2023-03-31 09:37:21 -0300

    exercicio soma h com n termos java

--- desafio-soma-h-n-termos/src/ExercicioSoma.java
+++ desafio-soma-h-n-termos/src/ExercicioSoma.java
@@ -1,4 +1,9 @@
-
+//Neste desafio, faça um programa que calcule o valor de H com N termos. 
+//Sendo, H = 1 + 1/2 + 1/3 + 1/4 + ... + 1/N. 
+//Entrada                    Saída
+//4                          2
+//8                          3
+//3                          2
 import java.util.Scanner;
 
 public class ExercicioSoma {
